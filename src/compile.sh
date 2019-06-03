e9b749fb363be427c0ce2ad44edb2009e1a6a062
Sapeint <sapeint@hashbang.sh>
2019-06-03 15:49:06 -0500

    Add in debug targets in Compile Script

--- src/compile.sh
+++ src/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 OUTPUT="sprocessman"
-gcc main.c ipc.c process/process.c user_processes/user_procs.c manager/manager.c -o "${OUTPUT}"
+DEBUG="-g -O0 -Wall -Wextra -pedantic -Werror -Wshadow"
+gcc ${DEBUG} main.c ipc.c process/process.c user_processes/user_procs.c manager/manager.c -o "${OUTPUT}"
