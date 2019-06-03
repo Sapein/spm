3c0988a44093dc7d0ba3d413aaeac2d9df4ae0ca
Sapeint <sapeint@hashbang.sh>
2019-06-03 16:17:10 -0500

    Changed gcc to cc for better support on FreeBSD

--- src/compile.sh
+++ src/compile.sh
@@ -2,4 +2,4 @@
 
 OUTPUT="sprocessman"
 DEBUG="-g -D_POSIX_C_SOURCE=200809 -std=c11 -O0 -Wall -Wextra -pedantic -Werror -Wshadow"
-gcc ${DEBUG} main.c ipc.c process/process.c user_processes/user_procs.c manager/manager.c -o "${OUTPUT}"
+cc ${DEBUG} main.c ipc.c process/process.c user_processes/user_procs.c manager/manager.c -o "${OUTPUT}"
