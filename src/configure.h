b248f2c1de1482654d27f3ad19f97c1309510654
Sapeint <sapeint@hashbang.sh>
2019-06-16 13:05:21 -0500

    Update configure.h

--- src/configure.h
+++ src/configure.h
@@ -1,6 +1,18 @@
 #ifndef S_SPMCONFIGURE
 #define S_SPMCONFIGURE
 
+/* General Management */
 #define TOTAL_MANAGED 10
-#define COMMUNICATION_FIFO_LOCATION ".spmfifo"
+#define COMMUNICATION_FIFO_LOCATION "/tmp/spm-ipc.fifo"
+
+/* Feature Options */
+/* TODO - Actually Implement these features */
+/* #define DOUBLE_FORK false /1* true = Double-Fork to Daemonize ; false = Do not double-fork *1/ */
+/* #define NAMED_PROCS false /1* true = Named Processes at runtime ; false = Processes don't have names at runtime *1/ */
+//#define IND_STOP false    /* true = Able to stop processes independently ; false = unable to stop processes independently. NOTE: Needs NAMED_PROCS set to work.*/
+
+/* Logging Information */
+#define LOGGING SYSLOG    /* printf = Use printfs to log ; syslog = use syslog interfaces ; custom = use custom ; off = no logging */
+#define LOGGING_NAME "Simple Process Manager"
+#define LOG_LEVEL WARN  /* Logging Level (INFO, DEBUG, WARN, ERROR). See logging.h for more details */
 #endif
