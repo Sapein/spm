2166f843727e09298ddcda7fb53dcd27feff5271
Sapeint <sapeint@hashbang.sh>
2019-06-27 23:57:02 -0500

    Update compile.sh to retain colors for formatting

--- src/compile.sh
+++ src/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 OUTPUT="sprocessman"
-DEBUG="-g -D_POSIX_C_SOURCE=200809 -std=c11 -O0 -Wall -Wextra -pedantic -Werror -Wshadow"
+DEBUG="-g -D_POSIX_C_SOURCE=200809 -std=c11 -O0 -Wall -Wextra -pedantic -Werror -Wshadow -fdiagnostics-color=always"
 cc ${DEBUG} main.c ipc.c process/process.c user_processes/user_procs.c manager/manager.c logging.c -o "${OUTPUT}"
